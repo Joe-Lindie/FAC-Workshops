3cf2e41e192b2799adaabaf7d40aa2f90a9526eb
Joe-Lindie <josephlindie@hotmail.co.uk>
2022-06-25 10:38:06 +0100

    Learn Unit Testing

--- learn-unit-testing/workshop/index.js
+++ learn-unit-testing/workshop/index.js
@@ -1,5 +1,30 @@
 // "pikachu" -> "https://pokeapi.co/api/v2/pikachu"
 
 function makeUrl(name) {
-  return "https://pokeapi.co/api/v2/" + name;
+  return "https://pokeapi.co/api/v2/" + name
 }
+
+/////////////////////////////////
+
+//searchParamsToObject Function//
+
+/////////////////////////////////
+
+function searchParamsToObject(string) {
+  const newObj = {}
+
+  const paramsArray = string.split("&")
+  //console.log(paramsArray)
+
+  for (const entry of paramsArray) {
+    const pairArray = entry.split("=")
+    const key = pairArray[0]
+    const value = pairArray[1]
+    newObj[key] = value
+  }
+
+  return newObj
+}
+
+searchParamsToObject("name=joe&email=hello@joelindie.com")
+//{name: 'joe', email: 'hello@joelindie.com'}
--- learn-unit-testing/workshop/index.test.js
+++ learn-unit-testing/workshop/index.test.js
@@ -4,19 +4,32 @@
 
 /////////////////////////////////
 
-test("makeURL() has correct pokemon name on poke API", () => {
-  const actual = makeUrl("pikachu")
-  const expected = "https://pokeapi.co/api/v2/pikachu"
+// test("makeURL() has correct pokemon name on poke API", () => {
+//   const actual = makeUrl("pikachu")
+//   const expected = "https://pokeapi.co/api/v2/pikachu"
 
-  const actualOne = makeUrl("pikachu")
-  const expectedOne = "https://pokeapi.co/api/v2/pikachu"
+//   const actualOne = makeUrl("pikachu")
+//   const expectedOne = "https://pokeapi.co/api/v2/pikachu"
 
-  equal(actual, expected)
-  equal(actualOne, expectedOne)
-})
+//   equal(actual, expected)
+//   equal(actualOne, expectedOne)
+// })
 
 /////////////////////////////////
 
 ///////// TEST TWO ////////////
+
+/////////////////////////////////
+
+test("searchParamsToObject() takes form-encoded string and returns object", () => {
+  const actual = searchParamsToObject("name=joe&email=hello@joelindie.com")
+  const expected = { name: "joe", email: "hello@joelindie.com" }
+
+  equal(actual.name, expected.name)
+})
+
+/////////////////////////////////
+
+///////// TEST THREE ////////////
 
 /////////////////////////////////
