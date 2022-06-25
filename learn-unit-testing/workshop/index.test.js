e90b135bfc3049f9b81132b53a4566b637908b2b
Joe-Lindie <josephlindie@hotmail.co.uk>
2022-06-25 13:47:46 +0100

    Leap year solution

--- learn-unit-testing/workshop/index.test.js
+++ learn-unit-testing/workshop/index.test.js
@@ -21,15 +21,37 @@
 
 /////////////////////////////////
 
-test("searchParamsToObject() takes form-encoded string and returns object", () => {
-  const actual = searchParamsToObject("name=joe&email=hello@joelindie.com")
-  const expected = { name: "joe", email: "hello@joelindie.com" }
+// test("searchParamsToObject() takes form-encoded string and returns object", () => {
+//   const actual = searchParamsToObject("name=joe&email=hello@joelindie.com")
+//   const expected = { name: "joe", email: "hello@joelindie.com" }
 
-  equal(actual.name, expected.name)
-})
+//   equal(actual.name, expected.name)
+// })
 
 /////////////////////////////////
 
 ///////// TEST THREE ////////////
 
 /////////////////////////////////
+
+test("isLeapYear(): Checks for valid leap years!", () => {
+  equal(isLeapYear(2020), "2020 IS a leap year!")
+  equal(isLeapYear(2000), "2000 IS a leap year!")
+  equal(isLeapYear(1960), "1960 IS a leap year!")
+})
+
+test("isLeapYear(): Checks for invalid leap years!", () => {
+  equal(isLeapYear(2019), "2019 is NOT leap year!")
+  equal(isLeapYear(1987), "1987 is NOT leap year!")
+  equal(isLeapYear(1905), "1905 is NOT leap year!")
+})
+
+test("isLeapYear(): Checks for negative years!", () => {
+  equal(isLeapYear(-1930), "You can not enter a negative year")
+  equal(isLeapYear(-1980), "You can not enter a negative year")
+})
+
+test("isLeapYear(): Checks for user entering strings", () => {
+  equal(isLeapYear("1950"), "Please enter a number")
+  equal(isLeapYear("Heyyy"), "Please enter a number")
+})
