0f4ff51e41dbb21435b18b024426327999f0ec1c
Joe-Lindie <josephlindie@hotmail.co.uk>
2022-06-29 16:18:45 +0100

    filter function TDD

--- tdd-array-methods/workshop/index.js
+++ tdd-array-methods/workshop/index.js
@@ -1,3 +1,16 @@
-function map(array) {
-  return array;
+/////////////////////////////////
+
+//////FUNCTION ONE ///////////
+
+/////////////////////////////////
+
+function filter(array, callback) {
+  const filteredArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    const testFn = callback(array[i])
+    filteredArray.push(testFn)
+  }
+
+  return filteredArray
 }
--- tdd-array-methods/workshop/index.test.js
+++ tdd-array-methods/workshop/index.test.js
@@ -1,11 +1,40 @@
-// testing map()
+/////////////////////////////////
 
-test("map() should return an array with the same number of elements", () => {
-  const result = map([1], () => {});
-  equal(result.length, 1);
-});
+//////TEST ONE ///////////
 
-test("map() should transform each element of the array using the fn argument", () => {
-  const result = map([1], (x) => x + 1);
-  equal(result[0], 2);
-});
+/////////////////////////////////
+
+test("filter function returns array ", () => {
+  const actual = filter([], () => {})
+  equal(Array.isArray(actual), true)
+})
+
+test("Tests if the first element in array passes the test", () => {
+  const actual = filter(["apple"], (word) => word.length > 2)
+  const expected = [true]
+  equal(actual[0], expected[0])
+})
+
+test("Tests if two element in array passes the test", () => {
+  const actual = filter(["apple", "elephant"], (word) => word.length > 6)
+  const expected = [false, true]
+
+  equal(actual[0], expected[0])
+  equal(actual[1], expected[1])
+})
+
+test("Tests if multiple array elements pass provided test", () => {
+  const wordsArray = [
+    "spray",
+    "limit",
+    "elite",
+    "exuberant",
+    "destruction",
+    "present",
+  ]
+  const actual = filter(wordsArray, (word) => word.length > 5)
+  const expected = [false, false, false, true, true, true]
+
+  equal(actual.length, expected.length)
+  equal(actual[0], expected[0])
+})
